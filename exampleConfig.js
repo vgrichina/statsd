a48254f84a2533997a73ede76d9c28f929ebb3f2
Vladimir Grichina <vgrichina@gmail.com>
2022-03-06 23:12:37 -0800

    Disable graphite in exampleConfig.js

--- exampleConfig.js
+++ exampleConfig.js
@@ -124,8 +124,8 @@ Optional Variables:
                          changes. The default is true. Set this to false to disable.
 */
 {
-  graphitePort: 2003
-, graphiteHost: "graphite.example.com"
-, port: 8125
-, backends: [ "./backends/graphite" ]
+//   graphitePort: 2003
+// , graphiteHost: "graphite.example.com"
+// , port: 8125
+// , backends: [ "./backends/graphite" ]
 }
